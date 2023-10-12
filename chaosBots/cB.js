3cc340dcca1b54d540612f64a3d92514fb4aa0c9
agSuperCoder123 <alex@ahoy.ooo>
2023-10-12 13:12:01 -0400

    gave basic functionality to a grid and an image change - looks very different tho than the figma and it's annoying

--- chaosBots/cB.js
+++ chaosBots/cB.js
@@ -1,17 +1,30 @@
-window.onload = function() {
-    fetch('chaosBots.json')
-        .then(response => response.json())
-        .then(bots => {
-            const botTable = document.getElementById('botTable');
-            bots.forEach(bot => {
-                const row = botTable.insertRow();
-                row.insertCell().textContent = bot.name;
-                row.insertCell().textContent = bot.title;
-                row.insertCell().textContent = bot.description;
-                const imgCell = row.insertCell();
-                const img = document.createElement('img');
-                img.src = bot.imgUrl;
-                imgCell.appendChild(img);
-            });
-        });
-}
+
+document.querySelectorAll('.gridItem').forEach(item => {
+    item.addEventListener('click', function() {
+        // Get the first column
+        const firstColumn = document.getElementById('leftBotContainer');
+
+        // Create a new element with the same content as the clicked item
+        const newItem = document.createElement('div');
+        newItem.classList.add('gridItem');
+        newItem.style.backgroundColor = this.style.backgroundColor;
+
+        // Add the new item to the first column
+        firstColumn.appendChild(newItem);
+    });
+});
+
+
+document.querySelectorAll('.gridItem img').forEach(img => {
+    img.addEventListener('click', function() {
+        let selectedBotImg = document.getElementById('selectedBotImg');
+        let imgElement = selectedBotImg.querySelector('img');
+
+        if (!imgElement) {
+            imgElement = document.createElement('img');
+            selectedBotImg.appendChild(imgElement);
+        }
+
+        imgElement.src = this.src;
+    });
+});
